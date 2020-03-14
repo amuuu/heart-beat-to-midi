68ca55d5a1b61de794236ee12a0febc91608f99a
Amirreza Amouie <aamouie01@gmail.com>
2020-03-14 10:16:06 -0700

    change stuff

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,29 +1,39 @@
 #include <SoftwareSerial.h>
-#define RX 10
+
+
+/***** FOR ESP8266 *****/
+#define RX 10 
 #define TX 11
-SoftwareSerial esp8266(RX,TX);
+SoftwareSerial esp(RX,TX);
+int ssid = "amu";
+int passwd = "amuamuamu";
+String data;
+String server = "yourServer";
+String uri = "yourURI";
+/***********************/
 
 
+/***** FOR HEART BEAT SENSOR *****/
 int PulseSensorPurplePin = 0; // analog pin 0
 int LED13 = 13;
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
 int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore.
+/*********************************/
 
 
 void setup() {
-  pinMode(LED13,OUTPUT);         // pin that will blink to your heartbeat!
-  Serial.begin(9600);         // Set's up Serial Communication at certain speed.
-  esp8266.begin(115200);
-  esp8266.println("AT");
-//  esp8266.println("AT+CWMODE=1");
-//  esp8266.println("AT+CWJAP=\"amu\",\"amuamuamu\"");
+  pinMode(LED13,OUTPUT);         // Blink the heart beat
+  Serial.begin(9600);
+  esp.begin(9600); // Serial connection for ESP8266
+
+  resetESP();
+  connectWifi();
 }
 void loop() {
 
   Signal = analogRead(PulseSensorPurplePin);
 
 //   Serial.println(Signal);
-//  esp8266.println("AT");
 
    if(Signal > Threshold){
      digitalWrite(LED13,HIGH);
@@ -31,8 +41,32 @@ void loop() {
      digitalWrite(LED13,LOW);
    }
 
+  delay(10);
 
-delay(10);
+}
 
 
+void resetESP() {
+  esp.println("AT+RST");
+  delay(1000);
+  if(esp.find("OK"))
+    Serial.println("Module Reset");
+    
+}
+
+
+void connectWifi() {
+  String setup_cmd = "AT+CWMODE=3"; 
+  esp.println(setup_cmd);
+  delay(1000);
+  
+  String connect_cmd = "AT+CWJAP=\"" +ssid+"\",\"" + password + "\"";
+  esp.println(connect_cmd);
+  delay(4000);
+  if(esp.find("OK")) {
+    Serial.println("Connected!");
+  }
+  else {
+    connectWifi();
+    Serial.println("Cannot connect to wifi"); }  
 }
--- pythonclient/main.py
+++ pythonclient/main.py
@@ -1,16 +1,17 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+print("Server is up...")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    while True:
-        try: 
-            s.connect((HOST, PORT))
-            # s.sendall(b'Hello, world')
-            data = s.recv(1024)
-            print('Received', repr(data))
-        
-        except socket.timeout:
-            print('Socket timeout')
-            break
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
