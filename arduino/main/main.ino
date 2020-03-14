bb3fd620266fc37fa00ff91f44223ef12a801dc8
Amirreza Amouie <aamouie01@gmail.com>
2020-03-14 10:57:58 -0700

    add comments for code

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,8 +1,7 @@
 #include <SoftwareSerial.h>
 
 
-
-
+/////////////////////////
 /***** FOR ESP8266 *****/
 #define RX 10 
 #define TX 11
@@ -13,20 +12,18 @@ String data;
 String server = "yourServer";
 String uri = "yourURI";
 /***********************/
+/////////////////////////
 
-
-
+///////////////////////////////////
+///////////////////////////////////
 /***** FOR HEART BEAT SENSOR *****/
 int PulseSensorPurplePin = 0; // analog pin 0
 int LED13 = 13;
 int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
 int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore.
 /*********************************/
-
-
-
-
-
+///////////////////////////////////
+///////////////////////////////////
 void setup() {
   pinMode(LED13,OUTPUT);  // Blink the heart beat
   Serial.begin(9600); // Normal serial connection baud rate
@@ -35,27 +32,21 @@ void setup() {
   resetESP();
   connectWifi();
 }
-
-
-
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 void loop() {
 
   int signal = readHeartBeats();
   
-//Serial.println(Signal);
+ //Serial.println(Signal);
   HandleLEDs(signal);
 
 
   delay(10);
 
 }
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 void resetESP() {
   esp.println("AT+RST");
   delay(1000);
@@ -63,12 +54,8 @@ void resetESP() {
     Serial.println("Module Reset");
     
 }
-
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 void connectWifi() {
   String setup_cmd = "AT+CWMODE=3"; 
   esp.println(setup_cmd);
@@ -85,13 +72,13 @@ void connectWifi() {
     Serial.println("Cannot connect to wifi");
   }  
 }
-
-
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 int readHeartBeats() {  
   return analogRead(PulseSensorPurplePin);
 }
-
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 void handleLEDs(int signal) {
   if(Signal > Threshold){
      digitalWrite(LED13,HIGH);
@@ -99,3 +86,4 @@ void handleLEDs(int signal) {
      digitalWrite(LED13,LOW);
    }
 }
+///////////////////////////////////////////////////////////////////////////
