7fe47f768b22cf1e3912c1e891f4ac81c658b0ac
Amirreza Amouie <aamouie01@gmail.com>
2020-03-17 01:25:04 -0700

    fix the result label padding issue

--- pythonserver/ui.py
+++ pythonserver/ui.py
@@ -4,34 +4,43 @@ window = Tk()
 window.title("Heart Beats to MIDI")
 window.geometry('350x200')
 padx, pady = 10, 5
+# window.pack(fill=BOTH, expand=True)
+
+frame1 = Frame(window)
+frame1.pack(fill=X)
 
 # scale name
-scale_name_lbl = Label(window, text="Scale Name")
+scale_name_lbl = Label(frame1, text="Scale Name")
 scale_name_lbl.grid(column=0, row=0, padx=padx, pady=pady)
-scale_name = Entry(window,width=10)
+scale_name = Entry(frame1,width=10)
 scale_name.grid(column=1, row=0, padx=padx, pady=pady)
 
 # scale type
-scale_type_lbl = Label(window, text="Scale Type")
+scale_type_lbl = Label(frame1, text="Scale Type")
 scale_type_lbl.grid(column=0, row=1, padx=padx, pady=pady)
-default_scale_type_txt = StringVar(window, value='1')
-scale_type = Entry(window,width=10, textvariable=default_scale_type_txt)
+default_scale_type_txt = StringVar(frame1, value='1')
+scale_type = Entry(frame1,width=10, textvariable=default_scale_type_txt)
 scale_type.grid(column=1, row=1, padx=padx, pady=pady)
 
 # MIDI port
-midi_port_lbl = Label(window, text="MIDI Port")
+midi_port_lbl = Label(frame1, text="MIDI Port")
 midi_port_lbl.grid(column=0, row=2, padx=padx, pady=pady)
-midi_port = Entry(window,width=10)
+default_midi_port_txt = StringVar(frame1, value='2')
+midi_port = Entry(frame1,width=10, textvariable=default_midi_port_txt)
 midi_port.grid(column=1, row=2, padx=padx, pady=pady)
 
 # MIDI channel
-midi_channel_lbl = Label(window, text="MIDI Channel")
+midi_channel_lbl = Label(frame1, text="MIDI Channel")
 midi_channel_lbl.grid(column=0, row=3, padx=padx, pady=pady)
-midi_channel = Entry(window,width=10)
+default_midi_channel_txt = StringVar(frame1, value='1')
+midi_channel = Entry(frame1,width=10, textvariable=default_midi_channel_txt)
 midi_channel.grid(column=1, row=3, padx=padx, pady=pady)
 
+frame2 = Frame(window)
+frame2.pack(fill=X)
+
 # result
-result_lbl = Label(window, text="")
+result_lbl = Label(frame2, text="", justify=LEFT)
 result_lbl.grid(column=0, row=5, padx=padx, pady=pady)
 
 
@@ -39,7 +48,7 @@ def clicked():
     res = "Welcome to " + scale_name.get() + " and " + scale_type.get() + " and " + midi_port.get() + " and " + midi_channel.get()
     result_lbl.configure(text= res)
 
-btn = Button(window, text="Click Me", command=clicked)
-btn.grid(column=0, row=4)
+btn = Button(frame1, text="Click Me", command=clicked)
+btn.grid(column=0, row=4, pady=pady)
 
 window.mainloop()
